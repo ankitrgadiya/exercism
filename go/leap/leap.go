3c4afa1b11da0eb76623b3a433988ed06e178641
Ankit R Gadiya <git@argp.in>
2018-11-28 10:07:31 +0530

    Go: Leap
    Refactored big boolen expression into conditionals

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -2,5 +2,11 @@ package leap
 
 // IsLeapYear : checks if year is leap year or no
 func IsLeapYear(year int) bool {
-	return (year % 4 == 0 && !(year % 100 == 0)) || (year % 400 == 0)
+	if year%4 == 0 && !(year%100 == 0) {
+		return true
+	} else if year%400 == 0 {
+		return true
+	}
+
+	return false
 }
