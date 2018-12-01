a515a669f4c0ce1081db7f8327bdaacc296e4c4c
Ankit R Gadiya <git@argp.in>
2018-12-01 19:28:07 +0530

    Go: Acronym

---
+++ go/acronym/acronym.go
@@ -0,0 +1,21 @@
+package acronym
+
+import "strings"
+
+// Abbreviate : Generates abbreviations for the given string
+func Abbreviate(s string) string {
+	s = strings.TrimSpace(s)
+	s = strings.Replace(s, "-", " ", -1)
+
+	split := strings.Split(s, " ")
+	if len(split) == 0 {
+		return ""
+	}
+
+	a := ""
+	for _, w := range split {
+		a += strings.ToUpper(string(w[0]))
+	}
+
+	return a
+}
