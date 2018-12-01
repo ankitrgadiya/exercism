7dbbb89179463cf295ac96c243a8163ce0d65981
Ankit R Gadiya <git@argp.in>
2018-12-02 00:13:43 +0530

    Go: Acronym
    Added conditionals to check the zero value of string.

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -5,7 +5,13 @@ import "strings"
 // Abbreviate : Generates abbreviations for the given string
 func Abbreviate(s string) string {
 	s = strings.TrimSpace(s)
+	if s == "" {
+		return ""
+	}
 	s = strings.Replace(s, "-", " ", -1)
+	if s == "" {
+		return ""
+	}
 
 	split := strings.Split(s, " ")
 	if len(split) == 0 {
@@ -14,7 +20,9 @@ func Abbreviate(s string) string {
 
 	a := ""
 	for _, w := range split {
-		a += string(w[0])
+		if w != "" {
+			a += string(w[0])
+		}
 	}
 
 	return strings.ToUpper(a)
