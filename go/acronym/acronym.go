280ffdc0d344597c031b0e7e95ac7c7fa1dc6130
Ankit R Gadiya <git@argp.in>
2018-12-02 00:36:43 +0530

    Go: Acronym
    Fields function does simplify the function a lot

--- go/acronym/acronym.go
+++ go/acronym/acronym.go
@@ -4,19 +4,8 @@ import "strings"
 
 // Abbreviate : Generates abbreviations for the given string
 func Abbreviate(s string) string {
-	s = strings.TrimSpace(s)
-	if s == "" {
-		return ""
-	}
 	s = strings.Replace(s, "-", " ", -1)
-	if s == "" {
-		return ""
-	}
-
-	split := strings.Split(s, " ")
-	if len(split) == 0 {
-		return ""
-	}
+	split := strings.Fields(s)
 
 	a := ""
 	for _, w := range split {
